7a5859321075914c2aa9fecca8234184ad6a003d
varun thummar <Varun>
2023-12-05 11:31:52 +0530

    start stop added

--- src/pages/Form/components/Navbar.js
+++ src/pages/Form/components/Navbar.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'; 
 
 import {GiHamburgerMenu} from 'react-icons/gi'
 
 const Navbar = () => {
+
+
+  const [signalId ,setSignalId]=useState(0)
+  const handleChange=(e)=>{
+    setSignalId(parseInt(e.target.value));
+  }
+
+  const startSignal= async ()=>{
+    const response = await fetch(`https://greenroad-gr.onrender.com/app/p1/on-signal/${signalId}`, {
+        method: 'GET',
+      });
+      if(response.ok)
+      {
+      alert(`SignalID ${signalId} start`);
+      }
+      else{
+        alert(`wrong signalid or server error`)
+      }
+  }
+
+  const stopSignal= async ()=>{
+    const response = await fetch(`https://greenroad-gr.onrender.com/app/p1/off-signal/${signalId}`, {
+        method: 'GET',
+      });
+      if(response.ok)
+      {
+      alert(`SignalID ${signalId} stop`);
+      }
+      else{
+        alert(`wrong signalid or server error`)
+      }
+  }
   return (
     <>
 
@@ -23,6 +55,11 @@ const Navbar = () => {
   <ul>
     <li>
       <a href="/">Add Circle</a>
+    </li>
+    <li>
+    <input type='number' onChange={handleChange} placeholder='circle id' className='mr-32 rounded-md h-8 text-2xl font-semibold'></input>
+    <button className='bg-white w-32 rounded-md font-semibold text-2xl mr-32' onClick={startSignal}>Start</button>
+    <button className='bg-white w-32 rounded-md font-semibold text-2xl' onClick={stopSignal}>Stop</button>
     </li>
   </ul>
 </div>
